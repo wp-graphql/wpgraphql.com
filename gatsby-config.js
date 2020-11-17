79d331d029128ec56a6a1fa31f8f38426c25f2b4
Jason Bahl <jasonbahl@mac.com>
2020-11-16 19:00:43 -0700

    - updating perPage - updating gatsby-source-wordpress experimental

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
                 url: process.env.WPGRAPHQL_URL,
                 verbose: true,
                 schema: {
-                  perPage: 50,
+                  perPage: 100,
                 },
                 debug: {
                     graphql: {
@@ -90,7 +90,7 @@ module.exports = {
                     },
                 },
                 develop: {
-                    hardCacheMediaFiles: false,
+                    hardCacheMediaFiles: true,
                     hardCacheData: false,
                 }
             },
