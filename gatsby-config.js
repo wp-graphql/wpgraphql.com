848e84251f0519a28c5a7046fef2334ea0565d33
Tyler Barnes <tylerdbarnes@gmail.com>
2021-02-02 15:31:46 -0800

    Revert "use latest wp source plugin alpha"
    This reverts commit 2aa53ca9e5ef9a10e09a8cebda1fa5b39b11d481.

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ const plugins = [
   `gatsby-transformer-sharp`,
   `gatsby-plugin-image`,
   {
-    resolve: `gatsby-source-wordpress`,
+    resolve: `gatsby-source-wordpress-experimental`,
     options: {
       url: process.env.WPGRAPHQL_URL,
       verbose: true,
