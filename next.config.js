801b9742eea5e5621aeb475ec4b0d543320d1256
AJ Moon <alex.jared.moon@gmail.com>
2022-08-25 23:32:13 +0000

    chore: package cleanup and updates

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const nextConfig = withBundleAnalyzer({
   experimental: { 
     runtime: "nodejs" 
   },
-  // had to set this to false in order to get @headless-ui components to work.
-  // @see: https://github.com/tailwindlabs/headlessui/issues/681#issuecomment-1086162507
-  reactStrictMode: false, 
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   images: {
     domains: [ 'secure.gravatar.com' ],
@@ -22,9 +19,6 @@ const nextConfig = withBundleAnalyzer({
   webpack(config, options) {
     config.resolve.alias = {
       ...config.resolve.alias,
-      // fixes next-mdx-remote: Package path ./jsx-runtime.js is not exported from package react
-      // https://github.com/hashicorp/next-mdx-remote/issues/237
-      "react/jsx-runtime.js": require.resolve("react/jsx-runtime"),
     };
     return config
   }
