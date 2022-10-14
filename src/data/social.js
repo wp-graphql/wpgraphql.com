1b5c76c44ad944cf1d92ea966cc819f55e46180c
AJ Moon <alex.jared.moon@gmail.com>
2022-10-14 16:02:20 +0000

    feat: social footer links

---
+++ src/data/social.js
@@ -0,0 +1,31 @@
+import { SiSlack, SiWordpress, SiYoutube, SiGithub, SiTwitter } from "react-icons/si"
+
+const socialLinks = [
+  {
+    name: "GitHub",
+    href: "https://github.com/wp-graphql/wp-graphql",
+    icon: SiGithub,
+  },
+  {
+    name: "WordPress Plugin",
+    href: "https://wordpress.org/plugins/wp-graphql",
+    icon: SiWordpress,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/wpgraphql",
+    icon: SiTwitter,
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/c/WPGraphQL",
+    icon: SiYoutube,
+  },
+  {
+    name: "Slack",
+    href: "https://join.slack.com/t/wp-graphql/shared_invite/zt-3vloo60z-PpJV2PFIwEathWDOxCTTLA",
+    icon: SiSlack,
+  },
+]
+
+export default socialLinks
