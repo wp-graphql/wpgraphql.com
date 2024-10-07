284ef28f6b550a4237536a835cc80af7f482d80f
Jason Bahl <jasonbahl@mac.com>
2024-10-07 12:52:14 -0600

    Update SiteFooter.js

--- src/components/Site/SiteFooter.js
+++ src/components/Site/SiteFooter.js
@@ -23,15 +23,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1 prose dark:prose-invert">
           <p className="text-center text-base text-navy dark:text-gray-300">
-            &copy; {year} WPGraphQL. All rights reserved. | Development
-            sponsored by{" "}
-            <a
-              href="https://www.wpengine.com/atlas"
-              target="_blank"
-              rel="noreferrer"
-            >
-              WP Engine
-            </a>
+            &copy; {year} WPGraphQL. All rights reserved.
           </p>
         </div>
       </div>
